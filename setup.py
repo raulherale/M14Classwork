795bf10631598a86745a2e9e8c8b33fa8a72c5f8
raulherale <51846188+raulherale@users.noreply.github.com>
2022-07-11 11:00:01 -0400

    M14 Classwork

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup , find_packages
 setup(name='Package',
       version='0.1',
       description='A demonstration package',
-      url='https://github.com/raulherale/M14Classwork',
+      url='https://github.com/raulherale/M14Classwork.git',
       author='Ashrith',
       author_email='something@gmail.com',
       license='MIT',
